13c01619356e36a5b2500b96a78c7b09e6de20a3
Joe Beuckman <joseph.beuckman@usbank.com>
2013-11-15 13:45:42 -0600

    checking for nil in characteristic desc

--- Classes/OrgBeuckmanTibleModule.m
+++ Classes/OrgBeuckmanTibleModule.m
@@ -497,11 +497,15 @@ static NSString *const kFlexServiceUUID = @"ADABFB00-6E7D-4601-BDA2-BFFAA68956BA
 
     [desc setObject:[self stringFromCBUUID:characteristic.UUID] forKey:@"uuid"];
 
-    NSString *valueString = [[NSString alloc] initWithData:characteristic.value encoding:NSUTF8StringEncoding];
+    NSString *valueString = [self hexRepresentationOfNSData:characteristic.value withSpaces:YES];
     NSLog(@"characteristic.value = %@", characteristic.value);
+
     if (valueString)
     [desc setObject:valueString forKey:@"value"];
-    [desc setObject:[characteristic.value description] forKey:@"description"];
+    
+    NSString *valueDesc = [characteristic.value description];
+    if (valueDesc)
+    [desc setObject:valueDesc forKey:@"description"];
     
     return desc;
 }
@@ -529,6 +533,31 @@ static NSString *const kFlexServiceUUID = @"ADABFB00-6E7D-4601-BDA2-BFFAA68956BA
     }
     
     return outputString;
+}
+
+-(NSString*)hexRepresentationOfNSData:(NSData *)data withSpaces:(BOOL)spaces
+{
+    const unsigned char* bytes = (const unsigned char*)[data bytes];
+    NSUInteger nbBytes = [data length];
+    //If spaces is true, insert a space every this many input bytes (twice this many output characters).
+    static const NSUInteger spaceEveryThisManyBytes = 4UL;
+    //If spaces is true, insert a line-break instead of a space every this many spaces.
+    static const NSUInteger lineBreakEveryThisManySpaces = 4UL;
+    const NSUInteger lineBreakEveryThisManyBytes = spaceEveryThisManyBytes * lineBreakEveryThisManySpaces;
+    NSUInteger strLen = 2*nbBytes + (spaces ? nbBytes/spaceEveryThisManyBytes : 0);
+    
+    NSMutableString* hex = [[NSMutableString alloc] initWithCapacity:strLen];
+    for(NSUInteger i=0; i<nbBytes; ) {
+        [hex appendFormat:@"%02X", bytes[i]];
+        //We need to increment here so that the every-n-bytes computations are right.
+        ++i;
+        
+        if (spaces) {
+            if (i % lineBreakEveryThisManyBytes == 0) [hex appendString:@"\n"];
+            else if (i % spaceEveryThisManyBytes == 0) [hex appendString:@" "];
+        }
+    }
+    return [hex autorelease];
 }
 
 @end
